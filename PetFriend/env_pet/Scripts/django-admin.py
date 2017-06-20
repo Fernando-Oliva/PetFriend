f1af0c0d33561ffbfd5184e872ca6e8af3503ff9
Fernando Oli <Fernando Oli>
2017-06-20 19:18:03 +0200

    Agregar archivos de proyecto.

---
+++ PetFriend/env_pet/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pccom\documents\visual studio 2017\Projects\PetFriend\PetFriend\env_pet\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
